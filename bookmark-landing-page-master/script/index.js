d3d12f885a0ef7d2d505d289dea077457bbc249d
Erick-Caua <146395495+Erick-Caua@users.noreply.github.com>
2025-01-04 15:43:47 -0300

    att

--- bookmark-landing-page-master/script/index.js
+++ bookmark-landing-page-master/script/index.js
@@ -2,6 +2,8 @@ const openNav = document.getElementById('open-nav');
 const closeNav = document.getElementById('close-nav');
 const cardNav = document.getElementById('card-nav');
 
+const duvidas = document.querySelectorAll('.duvida');
+
 //Abrir nav
 openNav.addEventListener("click", () => {
     cardNav.style.display = "flex"
@@ -21,3 +23,13 @@ closeNav.addEventListener("click", () => {
     cardNav.style.display = "none"
     openNav.style.display = "flex"
 })
+
+// mostrar perguntas frequentes
+
+duvidas.forEach(function (div) {
+    const paragrafoResposta = div.querySelector('.resposta');
+    div.addEventListener('click', function(){
+        paragrafoResposta.classList.toggle('hidden')
+    })
+    
+});
