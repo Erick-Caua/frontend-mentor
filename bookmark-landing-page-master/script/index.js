0c507eb1b7757e2fa16f34a4510a9ae8edf3abd7
Erick-Caua <146395495+Erick-Caua@users.noreply.github.com>
2025-01-04 17:09:15 -0300

    att

--- bookmark-landing-page-master/script/index.js
+++ bookmark-landing-page-master/script/index.js
@@ -2,6 +2,12 @@ const openNav = document.getElementById('open-nav');
 const closeNav = document.getElementById('close-nav');
 const cardNav = document.getElementById('card-nav');
 
+const article0 = document.getElementById('article-0');
+const article1 = document.getElementById('article-1');
+const article2 = document.getElementById('article-2');
+
+const escolhas = document.querySelectorAll('.escolha');
+
 const duvidas = document.querySelectorAll('.duvida');
 
 //Abrir nav
@@ -24,12 +30,43 @@ closeNav.addEventListener("click", () => {
     openNav.style.display = "flex"
 })
 
+//Simple - Speedy - Easy
+escolhas.forEach((botao) => {
+    botao.addEventListener('click', () => {
+        const valor = botao.value;
+
+        switch (valor) {
+            case 'simple':
+                article0.style.display = "flex"
+                article1.style.display = "none"
+                article2.style.display = "none"
+                break;
+            case 'speedy':
+                article0.style.display = "none"
+                article1.style.display = "flex"
+                article2.style.display = "none"
+                break;
+            case 'easy':
+                article0.style.display = "none"
+                article1.style.display = "none"
+                article2.style.display = "flex"
+                break;
+
+
+        }
+    })
+})
+
+
+
+
+
 // mostrar perguntas frequentes
 
 duvidas.forEach(function (div) {
     const paragrafoResposta = div.querySelector('.resposta');
-    div.addEventListener('click', function(){
+    div.addEventListener('click', () => {
         paragrafoResposta.classList.toggle('hidden')
     })
-    
+
 });
