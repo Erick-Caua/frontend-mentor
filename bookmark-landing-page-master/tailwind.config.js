fc246b1fcd12393e5037164747f33236d8e8cb5e
Erick-Caua <146395495+Erick-Caua@users.noreply.github.com>
2025-01-15 23:11:58 -0300

    att

--- bookmark-landing-page-master/tailwind.config.js
+++ bookmark-landing-page-master/tailwind.config.js
@@ -3,16 +3,16 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        Rubik:["Rubik", "serif"],
+      fontFamily: {
+        Rubik: ["Rubik", "serif"],
       }
     },
     colors: {
-      SoftBlue:'hsl(var(--SoftBlue))',
-      SoftRed:'hsl(var(--SoftRed))',
-      GrayishBlue:'hsl(var(--GrayishBlue))',
-      VeryDarkBlue:'hsl(var(--VeryDarkBlue))',
-      VeryDarkBlueNav:'hsl(var(--VeryDarkBlueNav))',
+      SoftBlue: 'hsl(var(--SoftBlue))',
+      SoftRed: 'hsl(var(--SoftRed))',
+      GrayishBlue: 'hsl(var(--GrayishBlue))',
+      VeryDarkBlue: 'hsl(var(--VeryDarkBlue))',
+      VeryDarkBlueNav: 'hsl(var(--VeryDarkBlueNav))',
       'white': '#ffffff',
       'white-50': '#f8fafc',
       transparent: 'transparent',
@@ -20,5 +20,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
 
