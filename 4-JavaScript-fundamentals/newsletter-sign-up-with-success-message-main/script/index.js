db2ec8eef241c4b968077141b445582318c5ee01
Erick-Caua <146395495+Erick-Caua@users.noreply.github.com>
2025-02-19 21:15:46 -0300

    at

--- 4-JavaScript-fundamentals/newsletter-sign-up-with-success-message-main/script/index.js
+++ 4-JavaScript-fundamentals/newsletter-sign-up-with-success-message-main/script/index.js
@@ -1,6 +1,9 @@
 const main = document.querySelector('.main')
 const form = document.getElementById('subscribeForm');
 
+const input = document.getElementById("email")
+const erroLabel = document.querySelector('.erroMen')
+
 const success = document.querySelector('.success')
 const dismiss = document.getElementById('dismiss')
 
@@ -13,16 +16,35 @@ form.addEventListener('submit', (event) => {
     const validarEmail = (email) => /^[^\s@]+@company\.com$/.test(email);
 
     if (validarEmail(emailValue)) { // Chama a função e verifica o resultado
+        input.classList.remove('erro');
+        // Remove a classe 'erro' do input
+        erroLabel.style.display = 'none';
+        // Oculta o label de erro
+
+        // Mostra a mensagem de sucesso
         success.style.display = 'flex'
         main.style.display = 'none';
+
+        // Atualiza o e-mail na mensagem de sucesso
+        document.querySelector('.success p span').textContent = emailValue;
     } else {
-        alert('Por favor, insira um endereço de e-mail válido.');
+        input.classList.add('erro'); 
+        // Adiciona a classe 'erro' ao input
+        erroLabel.style.display = 'block'; 
+        // Mostra o label de erro
     }
 });
 
 dismiss.addEventListener('click', () => {
     const cleanEmail = document.getElementById('email')
-    main.style.display = 'block'; // Mostra o elemento `main`
-    success.style.display = 'none'; // Oculta o elemento `success`
-    cleanEmail.value = "";
+    if (window.innerWidth > 768) {
+        main.style.display = 'flex'; // Mostra o elemento `main`
+        success.style.display = 'none'; // Oculta o elemento `success`
+        cleanEmail.value = "";
+    } else {
+        main.style.display = 'block'; // Mostra o elemento `main`
+        success.style.display = 'none'; // Oculta o elemento `success`
+        cleanEmail.value = "";
+    }
+
 });
